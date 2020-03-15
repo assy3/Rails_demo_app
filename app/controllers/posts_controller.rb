9c666403eab585eee3e799b872f0320f791ca8fa
assy3 <33861939+assy3@users.noreply.github.com>
2020-03-15 19:31:13 +0900

    新規投稿機能実装

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -9,6 +9,23 @@ class PostsController < ApplicationController
     #   "投稿一覧ページ作成中！",
     #   "おえんじゅん"
     # ]
-    @posts = Post.all
+    # @posts = Post.all
+    @posts = Post.all.order(created_at: :desc)
   end
+
+  def show
+    # @id = params[:id]
+    @post = Post.find_by(id: params[:id])
+  end
+
+  def new
+  end
+
+  def create
+    # フォームから送信されたデータを受け取り、保存する処理を追加してください
+    @post = Post.new(content: params[:content])
+    @post.save
+    redirect_to("/posts/index")
+  end
+
 end
