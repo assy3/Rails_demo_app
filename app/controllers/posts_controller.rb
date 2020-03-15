d22a9e9eae81855db774cae64df9e608da9a4f34
assy3 <33861939+assy3@users.noreply.github.com>
2020-03-15 20:57:37 +0900

    編集・削除機能実装

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,20 +1,9 @@
 class PostsController < ApplicationController
   def index
-    # viewで定義していた配列postsを@postsという変数名で定義してください
-    # @posts =
-    # [
-    #   "今日からProgateでRailsの勉強するよー！",
-    #   "投稿一覧ページ作成中！",
-    #   "今日からProgateでRailsの勉強するよー！",
-    #   "投稿一覧ページ作成中！",
-    #   "おえんじゅん"
-    # ]
-    # @posts = Post.all
     @posts = Post.all.order(created_at: :desc)
   end
 
   def show
-    # @id = params[:id]
     @post = Post.find_by(id: params[:id])
   end
 
@@ -22,9 +11,27 @@ class PostsController < ApplicationController
   end
 
   def create
-    # フォームから送信されたデータを受け取り、保存する処理を追加してください
     @post = Post.new(content: params[:content])
     @post.save
+    redirect_to("/posts/index")
+  end
+
+  def edit
+    @post = Post.find_by(id: params[:id])
+  end
+
+  def update
+    # updateアクションの中身を作成してください
+    @post = Post.find_by(id: params[:id])
+    @post.content = params[:content]
+    @post.save
+
+    redirect_to("/posts/index")
+  end
+
+  def destroy
+    @post = Post.find_by(id: params[:id])
+    @post.destroy
     redirect_to("/posts/index")
   end
 
