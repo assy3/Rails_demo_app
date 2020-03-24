5887d457c770b61d52b2a760b38b63386a925461
assy3 <33861939+assy3@users.noreply.github.com>
2020-03-24 19:27:07 +0900

    API弄って色々試していた

--- app/controllers/youtube_controller.rb
+++ app/controllers/youtube_controller.rb
@@ -3,6 +3,7 @@ class YoutubeController < ApplicationController
   # require 'active_support/all'
   GOOGLE_API_KEY = "AIzaSyBPVettgbut7W51LeYaoag1Ej45P6hSEfg"
 
+
   def find_videos(keyword, after: 1.months.ago, before: Time.now)
     service = Google::Apis::YoutubeV3::YouTubeService.new
     service.key = GOOGLE_API_KEY
@@ -18,9 +19,16 @@ class YoutubeController < ApplicationController
       published_before: before.iso8601
     }
     service.list_searches(:snippet, opt)
+
+  end
+
+
+  def get_channel_data(channel_id)
   end
 
   def index
     @youtube_data = find_videos('エガちゃんねる')
+
+    # @channel_data = get_channel_data('XXXXXXXXXXXX')
   end
 end
